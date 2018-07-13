d5f6ea0df65eb3eadf7e25779515a47fe3663a22
sanoel <noel.samuel.a@gmail.com>
2018-07-13 08:35:42 -0400

    connect method implementation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
 import { Provider } from 'cerebral'
 import oada from '@oada/oada-cache';
 
-export default Provider(oada);
+var connections = {};
+
+const connect = function connect(payload) {
+  let conn = oada.connect(payload);
+  connections[conn.connection_id] = conn;
+}
+
+const get = function get(payload) {
+  connections[payload.connection_id].get(payload);
+}
+
+const put = function put(payload) {
+  connections[payload.connection_id].put(payload);
+}
+
+const post = function post(payload) {
+  connections[payload.connection_id].post(payload);
+}
+
+
+const delete = function delete(payload) {
+  connections[payload.connection_id].delete(payload);
+}
+
+const resetCache = function clearCache(payload) {
+  connections[payload.connection_id].clearCachee(payload);
+}
+
+export default Provider({
+  connect,
+  get,
+  put,
+  post,
+  delete,
+  resetCache
+});
