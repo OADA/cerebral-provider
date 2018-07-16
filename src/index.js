4de50bc0913ea0796dd5da1bf23e33465876e15b
sanoel <noel.samuel.a@gmail.com>
2018-07-16 16:10:15 -0400

    provider methods weren't returning

--- src/index.js
+++ src/index.js
@@ -4,39 +4,46 @@ import uuid from 'uuid';
 var connections = {};
 
 const connect = function connect(payload) {
+  if (payload.connection_id && connections[payload.connection_id]) return Promise.resolve(connections[payload.domain]);
   return oada.connect(payload).then((conn) => {
-    conn.connection_id = uuid();
+    conn.connection_id = payload.connection_id || uuid();
     connections[conn.connection_id] = conn;
     return conn;
   })
 }
 
 const get = function get(payload) {
-  connections[payload.connection_id].get(payload);
+  return connections[payload.connection_id].get(payload);
 }
 
 const put = function put(payload) {
-  connections[payload.connection_id].put(payload);
+  return connections[payload.connection_id].put(payload);
 }
 
 const post = function post(payload) {
-  connections[payload.connection_id].post(payload);
+  return connections[payload.connection_id].post(payload);
 }
 
 
 const _delete = function _delete(payload) {
-  connections[payload.connection_id].delete(payload);
+  return connections[payload.connection_id].delete(payload);
 }
 
 
 const disconnect = function _disconnect(payload) {
-  connections[payload.connection_id].disconnect();
+  return connections[payload.connection_id].disconnect();
 }
 
 const resetCache = function resetCache(payload) {
-  connections[payload.connection_id].resetCache(payload);
+  return connections[payload.connection_id].resetCache(payload);
 }
 
 export default Provider({
   connect,
+  get, 
+  put,
+  post,
+  delete: _delete,
+  resetCache,
+  disconnect,
 });
