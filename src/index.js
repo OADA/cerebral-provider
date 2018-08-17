88b889a8018aa8faa857679993f89a32c09eccac
sanoel <noel.samuel.a@gmail.com>
2018-08-17 15:51:14 -0400

    watch implemented

--- src/index.js
+++ src/index.js
@@ -4,40 +4,53 @@ import uuid from 'uuid';
 var connections = {};
 
 const connect = function connect(args) {
-  if (args.connection_id && connections[args.connection_id]) return Promise.resolve(connections[args.connection_id]);
+  if (!args.connection_id) throw 'connection_id not supplied'
+  if (args.domain && connections[args.domain]) return Promise.resolve(connections[args.domain]);
   return oada.connect(args).then((conn) => {
-    conn.connection_id = args.connection_id || uuid();
-    connections[conn.connection_id] = conn;
+    connections[args.connection_id] = conn;
     return conn;
   })
 }
 
 const get = function get(args) {
-  if (args.watch && args.watch.signal) {
-    args.watch.func = this.context.controller.getSignal(args.watch.signal);
+  if (!args.connection_id) throw 'connection_id not supplied'
+  if (args.watch && args.watch.signals) {
+    let sigs = args.watch.signals.map((signal) => {
+      return this.context.controller.getSignal(signal);
+    })
+    args.watch.func = (payload) => {
+      sigs.forEach((signal) => {
+        signal(payload)
+      })
+    }
   }
   return connections[args.connection_id].get(args);
 }
 
 const put = function put(args) {
+  if (!args.connection_id) throw 'connection_id not supplied'
   return connections[args.connection_id].put(args);
 }
 
 const post = function post(args) {
+  if (!args.connection_id) throw 'connection_id not supplied'
   return connections[args.connection_id].post(args);
 }
 
 
 const _delete = function _delete(args) {
+  if (!args.connection_id) throw 'connection_id not supplied'
   return connections[args.connection_id].delete(args);
 }
 
 
 const disconnect = function _disconnect(args) {
+  if (!args.connection_id) throw 'connection_id not supplied'
   return connections[args.connection_id].disconnect();
 }
 
 const resetCache = function resetCache(args) {
+  if (!args.connection_id) throw 'connection_id not supplied'
   return connections[args.connection_id].resetCache(args);
 }
 
